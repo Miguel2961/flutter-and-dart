1a6f419d01d4034f29188dcb3c30b13f4c825ee5
jose-miguel <josemiguelatehortuaescobar@gmail.com>
2026-05-11 15:59:20 -0500

    feat: add number_selector component

--- Flutter/Proyect_IMC/imc_calculate/lib/Screens/imc_home_sreen.dart
+++ Flutter/Proyect_IMC/imc_calculate/lib/Screens/imc_home_sreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculate/Componets/gender_selector.dart';
 import 'package:imc_calculate/Componets/height_selector.dart';
+import 'package:imc_calculate/Componets/number_selector.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -8,14 +9,49 @@ class ImcHomeScreen extends StatefulWidget {
   @override
   State<ImcHomeScreen> createState() => _ImcHomeScreenState();
 }
-//La barra baja dice que es un fichero privado 
+
+//La barra baja dice que es un fichero privado
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectedAge = 20;
+  int selectedWeight = 80;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GenderSelector(),
-        HeightSelector()
+        HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: NumberSelector(
+                  title: "Peso",
+                  value: selectedWeight,
+                  onIncrement: () => setState(() {
+                    selectedWeight++;
+                  }),
+                  onDecrement: () => setState(() {
+                    selectedWeight--;
+                  }),
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: NumberSelector(
+                  title: "Edad",
+                  value: selectedAge,
+                  onIncrement: () => setState(() {
+                    selectedAge++;
+                  }),
+                  onDecrement: () => setState(() {
+                    selectedAge--;
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
